a564b872697d6953aa677f3048272449f6af9a29
Bikash Chauhan <chauhanbikash789@gmail.com>
2022-07-14 21:14:55 +0545

    Code refactored

---
+++ index.js
@@ -0,0 +1,240 @@
+songs_details = [
+  {
+    source: [
+      "https://dl.dropbox.com/s/6ybjlgoq2d8x0e7/%23alavaikunthapurramuloo%20-%20ramuloo%20ramulaa%20full%20video%20songallu%20arjuntrivikramthaman%20s.mp3",
+      "https://dl.dropbox.com/s/mpxyl82w345ffnp/yenti%20yenti%20full%20video%20song%20%7C%7C%20geetha%20govindam%20songs%20%7C%7C%20vijay%20devarakonda%2C%20rashmika%20mandanna.mp3",
+      "https://dl.dropbox.com/s/87n64mw1vmwxi86/bambholle%20-%20full%20video%20%7C%20laxmii%20%7C%20akshay%20kumar%20%7C%20viruss%20%7C%20ullumanati.mp3",
+      "https://dl.dropbox.com/s/l1gff5z126u81wo/ritviz%20-%20liggi%20%5Bofficial%20music%20video%5D.mp3",
+      "https://dl.dropbox.com/s/rrut8qo6jje3b8a/baarishein%28studio%29%20-%20anuv%20jain%20%28lyrics%29%20%281%29.mp3",
+      "https://dl.dropbox.com/s/75xd81jgvqpnj50/taaron%20ke%20shehar%20song%20neha%20kakkar%2C%20sunny%20kaushaljubin%20nautiyal%2Cjaanibhushan%20kumararvindr%20k.mp3",
+      "https://dl.dropbox.com/s/7criq09w8kns5ix/sawan%20mein%20lag%20gayi%20aag%20-%20ginny%20weds%20sunny%20%7C%20yami%2C%20vikrant%20%7C%20mika%2C%20neha%20%26amp%3B%20badshah%20%7C%20payal%20d%2C%20moh.mp3",
+      "https://dl.dropbox.com/s/rdujjxfwdf9bnuk/BurjKhalifa%20-%20Laxmmi%20Bomb.mp3",
+      "https://dl.dropbox.com/s/9m1xn4e5byiqnoh/Closer%20-%20The%20ChainsmokersHalsey%20%28Alex%20Goot%20%26%20ATC%29.mp3",
+      "https://dl.dropbox.com/s/5c5olzn5ybbvrb6/The%20Software%20Developer%20Short%20Film%20LOVE%20BGM%20The%20Software%20DevLOVEper%20BGM%20Vaishnavi%20Chaitanya.mp3",
+      "https://dl.dropbox.com/s/qc449himgyczbc9/Dil%20Ke%20Dastakk%20-%20Please%20Find%20Attached%20%5BLYRICS%5D.mp3",
+      "https://dl.dropbox.com/s/zh2ufdjqxrzm4ff/Zaeden%20-%20dooriyan%20%28Official%20Music%20Video%29%20Aashna%20Hegde.mp3",
+      "https://dl.dropbox.com/s/niikv35xu7sdwov/Heelein%20Toot%20Gayi%28AUDIO%29Indoo%20Ki%20JawaniBadshah%2CGuru%20Randhawa%2CKiara%20Advani%2CAditya%20Seal%2CAastha%20Gi.mp3",
+      "https://dl.dropbox.com/s/4cnw5ae4aowrask/First%20Kiss%3A%20Yo%20Yo%20Honey%20Singh%20Ft.%20Ipsitaa%20%7C%20Bhushan%20Kumar%20%7C%20Lil%20Golu%2C%20Singhsta%2C%20Hommie%20D%2C%20DirGif.mp3",
+    ],
+  },
+
+  {
+    album_art: [
+      "https://c-sf.smule.com/rs-s-sf-2/arr/77/93/4d531ad8-4f24-40cf-9bf1-aa7fd18e4f55_1024.jpg",
+      "https://i.ytimg.com/vi/dAUHa5K38t4/maxresdefault.jpg",
+      "https://a10.gaanacdn.com/gn_img/song/kGxbnw0Ky4/xbnmpDJ0Wy/size_xxl_1604405606.webp",
+      "https://i.ytimg.com/vi/Ikul1Euth08/maxresdefault.jpg",
+      "https://i.ytimg.com/vi/TJcxtrBozuw/maxresdefault.jpg",
+      "https://a10.gaanacdn.com/gn_img/albums/0wrb4kNWLg/wrb476lyWL/size_xxl.jpg",
+      "https://sm.mashable.com/mashable_in/seo/default/sawan-mein_akzu.jpg",
+      "https://www.bollywoodbubble.com/wp-content/uploads/2020/10/BurjKhalifa-Song-2-1280x720.jpg",
+      "https://a10.gaanacdn.com/images/albums/93/1771493/crop_175x175_1771493.jpg",
+      "https://static.skillshare.com/uploads/project/7ef9e8338e037a5db53dc2a634b5c962/e9376c0b",
+      "https://www.lyricsgoo.com/wp-content/uploads/2021/01/Dil-Ke-Dastakk-Lyrics-Karthik-Rao-x-Shilpa-Surroch.jpg",
+      "https://www.lyricsgoo.com/wp-content/uploads/2020/06/Dooriyan-Lyrics-Zaeden.jpg",
+      "https://1.bp.blogspot.com/-PCUpimKLfsM/X7_ooW1yP2I/AAAAAAAAKHM/U9vRiw3L6QcBUw_3HaUo_8Vn32CUcR2cQCNcBGAsYHQ/s1280/Heelein%2BToot%2BGayi%2BSong%2BImage%2BFeatures%2BBadshah.jpg",
+      "https://a10.gaanacdn.com/images/albums/56/3547856/crop_480x480_3547856.jpg",
+    ],
+  },
+
+  {
+    title: [
+      "Ramuloo Ramulaa",
+      "Yenti Yenti",
+      "Bam Bholle",
+      "Liggi- Rivitz",
+      "Baarishein",
+      "Taaron Ke Shehar",
+      "Sawan Mein Lag Gayi Aag",
+      "BurjKhalifa-Laxmmi Bomb",
+      "Closer-The Chainsmokers",
+      "Love BGM.mp3",
+      "Dil Ke Dastakk",
+      "Zaeden-Dooriyan",
+      "Heelein Toot Gayi",
+      "First Kiss",
+    ],
+  },
+];
+
+aart = document.getElementById("album-art");
+plays = document.getElementById("play");
+music_ = document.getElementById("music");
+src = document.getElementById("source");
+titl = document.getElementById("title");
+d = document.getElementById("duration");
+ct = document.getElementById("current_time");
+isPlaying = false;
+
+cindex = 0;
+function set_album(i = 0) {
+  aart.setAttribute("src", songs_details[1]["album_art"][i]);
+}
+function load_music(i = 0) {
+  src.setAttribute("src", songs_details[0]["source"][i]);
+  music_.load();
+  set_album(i);
+}
+
+function add_class() {
+  aart.classList.add("rotate");
+}
+function remove_class() {
+  aart.classList.remove("rotate");
+}
+function title(i = 0) {
+  titl.innerHTML = songs_details[2]["title"][i];
+}
+
+function play() {
+  if (!isPlaying) {
+    music_.play();
+    plays.innerHTML = "〓";
+    plays.style.transform = "rotate(90deg)";
+    add_class();
+    isPlaying = true;
+  } else {
+    music_.pause();
+    plays.innerHTML = "▶︎";
+    plays.style.transform = "rotate(0deg)";
+    remove_class();
+    isPlaying = false;
+  }
+}
+function next() {
+  cindex = (cindex + 1) % songs_details[0]["source"].length;
+  load_music(cindex);
+  isPlaying = false;
+  play();
+  title(cindex);
+  playing_now();
+}
+function previous() {
+  cindex =
+    (cindex - 1 + songs_details[0]["source"].length) %
+    songs_details[0]["source"].length;
+  load_music(cindex);
+  isPlaying = false;
+  play();
+  title(cindex);
+  playing_now();
+}
+
+load_music(cindex);
+title(cindex);
+
+function adding_upcom() {
+  var x;
+  for (x = cindex; x < songs_details[2]["title"].length; x++) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(songs_details[2]["title"][x]);
+    node.appendChild(textnode);
+    document.getElementById("list").appendChild(node);
+  }
+}
+adding_upcom();
+function playing_now() {
+  var z = document.getElementById("list").children;
+  z[cindex].style.color = "white";
+  var y;
+  for (y = 0; y < songs_details[2]["title"].length; y++) {
+    if (y != cindex) {
+      z[y].style.color = "black";
+    }
+  }
+}
+
+playing_now();
+
+var list = document.getElementById("list").children;
+var z = Object.values(list);
+for (var y = 0; y < z.length; y++) {
+  var i;
+  z[y].addEventListener("click", () => {
+    i = event.target;
+    i = i.innerHTML;
+    cindex = songs_details[2]["title"].indexOf(i);
+    load_music(cindex);
+    set_album(cindex);
+    title(cindex);
+    isPlaying = false;
+    play();
+    playing_now();
+  });
+}
+
+function convert(sec_value) {
+  sec_value = parseInt(sec_value);
+  let min = 0;
+  let sec = 0;
+  if (sec_value < 60) {
+    sec = sec_value;
+  } else if (sec_value >= 60) {
+    min = Math.floor(sec_value / 60);
+    sec = sec_value % 60;
+  }
+  d = document.getElementById("duration");
+  document.getElementById("seek").max = sec_value;
+  d.innerHTML = `${min}` + ":" + `${sec}`;
+}
+
+music_.addEventListener("ended", (event) => {
+  next();
+});
+
+music_.addEventListener("timeupdate", (event) => {
+  const { currentTime, duration } = event.srcElement;
+  document.getElementById("seek").max = music_.duration;
+  document.getElementById("seek").value = `${currentTime}`;
+
+  convert(duration);
+  q = Math.floor(currentTime);
+  if (q < 10) {
+    ct.innerHTML = "00:" + "0" + `${q}`;
+  } else if ((q >= 10) & (q < 60)) {
+    ct.innerHTML = "00:" + `${q}`;
+  } else if (q >= 60) {
+    m = Math.floor(q / 60);
+    s = q % 60;
+    ct.innerHTML = "0" + `${m}` + ":" + `${String(s).padStart(2, "0")}`;
+  }
+});
+globalThis;
+showing = false;
+
+function show() {
+  if (!showing) {
+    document.getElementById("upcoming").style.visibility = "visible";
+    showing = true;
+  } else {
+    document.getElementById("upcoming").style.visibility = "hidden";
+    showing = false;
+  }
+}
+
+//Generate color codes
+
+const generateColor = () => {
+  var color1 = Math.floor(Math.random() * 255 + 1);
+  var color2 = Math.floor(Math.random() * 255 + 1);
+  var color3 = Math.floor(Math.random() * 255 + 1);
+  let codes = [];
+  codes.push(color1);
+  codes.push(color2);
+  codes.push(color3);
+  codes = codes.toLocaleString();
+  return `rgb(${codes})`;
+};
+const setBackground = () => {
+  let element = document.getElementById("bg");
+  element.style.background = `linear-gradient(90deg, ${generateColor()}, ${generateColor()})`;
+};
+
+setInterval(() => {
+  setBackground();
+}, 3000);
+
+//show seeked position
+const showSeekedPos = () => {
+  music_.currentTime = document.getElementById("seek").value;
+};
